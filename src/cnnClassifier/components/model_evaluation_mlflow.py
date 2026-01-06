8469b67e0fef7099c805e21d6ae64c902d333366
vikashkumardatascience007 <vikash.datascience.007@gmail.com>
2026-01-07 00:14:07 +0530

    code

--- src/cnnClassifier/components/model_evaluation_mlflow.py
+++ src/cnnClassifier/components/model_evaluation_mlflow.py
@@ -63,12 +63,12 @@ class Evaluation:
                 {"loss": self.score[0], "accuracy": self.score[1]}
             )
             # Model registry does not work with file store
-            if tracking_url_type_store != "file":
+            # if tracking_url_type_store != "file":
 
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.keras.log_model(self.model, "model", registered_model_name="VGG16Model")
-            else:
-                mlflow.keras.log_model(self.model, "model")
+            #     # Register the model
+            #     # There are other ways to use the Model Registry, which depends on the use case,
+            #     # please refer to the doc for more information:
+            #     # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            #     mlflow.keras.log_model(self.model, "model", registered_model_name="VGG16Model")
+            # else:
+            mlflow.keras.log_model(self.model, "model")
